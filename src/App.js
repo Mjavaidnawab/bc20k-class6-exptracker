b5c12d49069ba61c3faa247d289ba10fcd3f7c60
Mjavaidnawab <57194178+Mjavaidnawab@users.noreply.github.com>
2020-09-23 17:31:30 +0400

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Child from './child';
+import './App.css';
+import {TransactionProvider} from './transContext';
+
+function App() {
+  return (
+    <TransactionProvider >
+      <Child />
+    </TransactionProvider>
+  );
+}
+
+export default App;
